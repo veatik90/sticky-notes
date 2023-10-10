06b1150ca0b056b83cd9c9f3a80d64a4b702b512
Ilya Kitayev <veatik90@gmail.com>
2023-10-10 13:45:17 +0200

    feature: add uncontrolled text area to notes

--- src/components/Note/index.tsx
+++ src/components/Note/index.tsx
@@ -5,7 +5,7 @@ import styles from "./styles.module.css";
 export const Note: FC = () => {
   return (
     <div className={styles.noteContainer}>
-      <span></span>
+      <textarea />
     </div>
   );
 };
